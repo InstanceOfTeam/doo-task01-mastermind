a2c6e3f2b5f83e359b559ab3873a7cb9091bbd12
Rubén <rbadenas@agroxarxa.com>
2020-11-24 00:09:17 +0100

    issue/restorePreviousCodeFromCombination

--- src/src/Combination.java
+++ src/src/Combination.java
@@ -1,17 +1,12 @@
 
 abstract class Combination {
-	
-	public static final int LENGTH = 4;
-	protected Color[] colors;
-	
-	protected Combination(String combination) {
-		this.colors = new Color[LENGTH];
 
-		for(int i=0; i<LENGTH; i++){
-			this.colors[i] = Color.getColorByChar(combination.charAt(i));
-		}
+	protected Color[] colors;
+
+	protected Combination() {
+
 	}
-	
+
 	@Override
 	public String toString() {
 		return null;
