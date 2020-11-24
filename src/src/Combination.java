be7222ef45c664a116dfc39b2e1fba1c93c88fc1
Alberto Morales Fernandez <kodenix@gmail.com>
2020-11-24 10:04:45 +0100

    Implemented Combination

--- src/src/Combination.java
+++ src/src/Combination.java
@@ -1,15 +1,22 @@
 
 abstract class Combination {
 
+	protected final int LENGTH = 4;
 	protected Color[] colors;
 
 	protected Combination() {
-
+		this.colors = new Color[LENGTH];
 	}
 
 	@Override
 	public String toString() {
-		return null;
+		String textColors = "";
+		
+		for (int i=0; i < colors.length; i++) {
+			textColors+=colors[i].toString();
+		}
+		
+		return textColors;
 	}
 
 }
