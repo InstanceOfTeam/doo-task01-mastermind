20093d7e57e8b0c1c12c6089b42d406daf2daead
japc <japc.grafico@gmail.com>
2020-11-24 15:57:39 +0100

    Implement Attempt class

--- src/src/Attempt.java
+++ src/src/Attempt.java
@@ -1,18 +1,22 @@
 
 class Attempt {
+	private ProposedCombination proposedCombination;
+	private Result result;
 
 	public Attempt(SecretCombination secretCombination) {
-		// TODO Auto-generated constructor stub
+		this.proposedCombination = new ProposedCombination();
+		this.result = secretCombination.evalueCombination(this.proposedCombination);
 	}
 
 	public boolean isWinner() {
-		// TODO Auto-generated method stub
+		if (this.result.getBlacks() == 4) {
+			return true;
+		}
 		return false;
 	}
-	
+
 	@Override
 	public String toString() {
-		return null;
+		return this.proposedCombination.toString();
 	}
-
 }
