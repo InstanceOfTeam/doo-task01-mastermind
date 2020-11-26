faddc75ad6d7e64ee7725724ff6130aba7a48df1
mpagola <72516648+mpagolae@users.noreply.github.com>
2020-11-26 18:38:26 +0100

    Update Mastermind.java

--- src/src/Mastermind.java
+++ src/src/Mastermind.java
@@ -3,7 +3,8 @@ import java.util.ArrayList;
 class Mastermind {
 	private ArrayList<Attempt> attempts = new ArrayList<Attempt>();
 	private SecretCombination secretCombination;
-	private static final int NUM_ATTEMPS = 10;
+	private final int NUM_ATTEMPS = 10;
+	private Object writeConsole;
 	
 
 	public Mastermind() {
@@ -11,28 +12,32 @@ class Mastermind {
 	}
 	
 	public void play() {
+		this.writeConsole();
 		Console console = new Console();
-		int nAttemps = 0;
-		console.write(nAttemps + " attemp(s):");
-		console.write("xxxx");
+
 		do {
 			attempts.add(new Attempt(secretCombination));
-			console.write(nAttemps + 1  + " attemp(s):");
-			console.write("xxxx");
+			this.writeConsole();
 			for (int i=0; i < attempts.size(); i++) {
 				console.write(attempts.get(i).toString());
 			}
-			nAttemps += 1;
-
-		} while (nAttemps <= NUM_ATTEMPS && !attempts.get(nAttemps-1).isWinner());
+		} while (attempts.size() <= NUM_ATTEMPS && !attempts.get(attempts.size()-1).isWinner());
 		
-		if (attempts.get(nAttemps-1).isWinner()) {
+		if (attempts.get(attempts.size()-1).isWinner()) {
 			console.write("You've won!!!");
 			} else {
 				console.write("You've lost");
 		}
 	}
 
+	public void writeConsole() {
+		Console console = new Console();
+		
+		console.write(attempts.size() + " attemp(s):");
+		console.write(this.secretCombination.toString());	
+	}
+	
+	
 	public static void main(String[] args) {	
 		do {
 			new Mastermind().play();
